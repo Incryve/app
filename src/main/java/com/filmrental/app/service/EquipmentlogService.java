2a1e52abeb2682d22d62b9504dc72a808efc9dd9
Incryve <67076293+Incryve@users.noreply.github.com>
2023-11-20 11:55:06 -0300

    hotfix equipmentlog added methods

--- src/main/java/com/filmrental/app/service/EquipmentlogService.java
+++ src/main/java/com/filmrental/app/service/EquipmentlogService.java
@@ -2,7 +2,12 @@ package com.filmrental.app.service;
 
 import com.filmrental.app.model.Equipmentlog;
 
+import java.util.List;
+
 public interface EquipmentlogService {
     Equipmentlog findById(Long id);
     Equipmentlog create(Equipmentlog equipmentLogToCreate);
+    Equipmentlog update(Long id, Equipmentlog equipmentLog);
+    void delete(Long id);
+    List<Equipmentlog> findAll();
 }
--- src/main/java/com/filmrental/app/service/impl/EquipmentlogServiceImpl.java
+++ src/main/java/com/filmrental/app/service/impl/EquipmentlogServiceImpl.java
@@ -5,6 +5,7 @@ import com.filmrental.app.repository.EquipmentlogRepository;
 import com.filmrental.app.service.EquipmentlogService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 
 @Service
@@ -27,5 +28,26 @@ public class EquipmentlogServiceImpl implements EquipmentlogService {
             throw new IllegalArgumentException("The equipment log id already exists.");
         }
         return equipmentlogRepository.save(equipmentLogToCreate);
+    }
+
+    @Override
+    public Equipmentlog update(Long id, Equipmentlog equipmentLog) {
+        return equipmentlogRepository.findById(id)
+                .map(existingEquipmentLog -> {
+                    existingEquipmentLog.setLogText(equipmentLog.getLogText());
+                    existingEquipmentLog.setLogDate(equipmentLog.getLogDate());
+                    existingEquipmentLog.setEquipment(equipmentLog.getEquipment());
+                    return equipmentlogRepository.save(existingEquipmentLog);
+                }).orElseThrow(() -> new NoSuchElementException("Equipment log not found with id: " + id));
+    }
+
+    @Override
+    public void delete(Long id) {
+        equipmentlogRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Equipmentlog> findAll() {
+        return equipmentlogRepository.findAll();
     }
 }
